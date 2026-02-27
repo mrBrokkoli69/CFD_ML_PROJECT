536decae303caf7f889cd21cfabcea4e985b4677
PolonskiyMax <dopaccaunt3@mail.ru>
2026-02-27 10:31:24 +0300

    Add screen render with # and .

--- src/mask_editor/editor.cpp
+++ src/mask_editor/editor.cpp
@@ -1,20 +1,37 @@
 #include <ncurses.h>
 #include "editor.h"
 
+void drawMask(const Mask& mask, int cursorX, int cursorY) {
+	 for (int y = 0; y < mask.HEIGHT; y++) {
+       	 for (int x = 0; x < mask.WIDTH; x++) {
+		if (mask.isSolid(x,y)) {
+			mvaddch(y+1,x+1,'#');
+		}
+		else {
+			mvaddch(y+1,x+1,'.');
+		}
+        }
+    }
+    
+    refresh();
+
+}
 
 int main() {
 	Mask mask;
-	mask.setSolid(10,5,true);
-	if (mask.isSolid(10,5)) {}
-	mask.setSolid(10,5,false);	
-
-
 
 	initscr();
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
-	printw("Mask Editor v1.0\n");
+	
+	mask.setSolid(5,6,true);
+	mask.setSolid(5,7,true);
+	mask.setSolid(5,8,true);
+
+	drawMask(mask,0,0);
+
+	printw("\nMask Editor v1.0\n");
 	printw("Press q to quit");
 	refresh();
 	int ch;

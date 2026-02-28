1aaf714a287e2c2e205fc0a116836e61106ab153
PolonskiyMax <dopaccaunt3@mail.ru>
2026-02-28 17:59:26 +0300

    Add changing of cursor by keyboard

--- src/mask_editor/editor.cpp
+++ src/mask_editor/editor.cpp
@@ -12,8 +12,8 @@ void drawMask(const Mask& mask, int cursorX, int cursorY) {
 		}
         }
     }
-    
-    refresh();
+	mvaddch(cursorY + 1, cursorX + 1, 'X');    
+	refresh();
 
 }
 
@@ -25,18 +25,57 @@ int main() {
 	keypad(stdscr, TRUE);
 	noecho();
 	
-	mask.setSolid(5,6,true);
-	mask.setSolid(5,7,true);
-	mask.setSolid(5,8,true);
+	int cursorX = mask.WIDTH / 2 ;
+	int cursorY = mask.HEIGHT / 2 ;
 
-	drawMask(mask,0,0);
 
-	printw("\nMask Editor v1.0\n");
-	printw("Press q to quit");
-	refresh();
+
 	int ch;
-	while((ch = getch()) != 'q')
-        { }
+	while((true))
+        {
+		drawMask(mask, cursorX, cursorY);
+		
+		move(mask.HEIGHT + 2, 0) ;	
+		printw("Mask editor v1.0\n");
+		printw("Press q to quit");
+		refresh();
+		
+
+		ch = getch();
+		if(ch == 'q') {
+			break;
+		}
+		
+		switch(ch) {
+			case KEY_UP:
+				if(cursorY>0) cursorY--;
+				break;
+			case KEY_DOWN:
+				if(cursorY < mask.HEIGHT - 1) cursorY++;
+				break;
+			case KEY_LEFT:
+				if(cursorX > 0) cursorX--;
+				break;
+			case KEY_RIGHT:
+				if(cursorX < mask.WIDTH - 1) cursorX++;
+				break;
+			case 'w':
+				if(cursorY>0) cursorY--;
+				break;
+			case 's':
+				if(cursorY < mask.HEIGHT - 1) cursorY++;
+				break;
+			case 'a':
+				if(cursorX > 0) cursorX--;
+				break;
+			case 'd':
+				if(cursorX < mask.WIDTH - 1) cursorX++;
+				break;
+
+		}
+
+	}
+
 	endwin();
 	return 0;	
 }

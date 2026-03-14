50050613da40674dcaafb85d896a9636eeeec808
PolonskiyMax <dopaccaunt3@mail.ru>
2026-03-14 16:07:40 +0300

    Add massives for velocities and weights in lbm model

--- src/lbm/lbm_core.h
+++ src/lbm/lbm_core.h
@@ -1,13 +1,44 @@
 #ifndef LBM_CORE_H
 #define LBM_CORE_H
 
+static const int D = 2;
+static const int Q = 9;
 
+static const int cx[Q] = {
+	0,
+	1,
+	0,
+	-1,
+	0,
+	1,
+	-1,
+	-1,
+	1
+};
 
+static const int cy[Q] = {
+	0,
+	0,
+	1,
+	0,
+	-1,
+	1,
+	1,
+	-1,
+	-1
+};
 
-
-
-
-
+static const int w[Q] = {
+	4.0/9.0,
+	1.0/9.0,
+	1.0/9.0,
+	1.0/9.0,
+	1.0/9.0,
+	1.0/36.0,
+	1.0/36.0,
+	1.0/36.0,
+	1.0/36.0
+};
 
 
 

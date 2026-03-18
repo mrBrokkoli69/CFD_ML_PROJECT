0c6e136eef708f57e7a7143de319fa6d6a780aaa
PolonskiyMax <dopaccaunt3@mail.ru>
2026-03-18 22:50:44 +0300

    Add LBMField Structure

--- src/lbm/lbm_core.h
+++ src/lbm/lbm_core.h
@@ -1,6 +1,11 @@
 #ifndef LBM_CORE_H
 #define LBM_CORE_H
 
+
+#include <cmath>
+#include <vector>
+
+
 static const int D = 2;
 static const int Q = 9;
 
@@ -40,8 +45,23 @@ static const int w[Q] = {
 	1.0/36.0
 };
 
+static const int CS2 = 1.0/3.0;
+static const int CS = std::sqrt(CS2);
 
+struct LBMField {
+	int nx;
+	int ny;
 
+	std::vector<std::vector<std::vector<double>>> f; //f[i][y][x] -- трехмерный массив функции распределения по каждому направлению в узлах
+	
+	std::vector<std::vector<double>> rho;
+	std::vector<std::vector<double>> ux;
+	std::vector<std::vector<double>> uy;
+	
+	LBMField(int ny, int nx);
+	
+
+};
 
 
 
